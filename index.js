d90ed51e9b68e7747567fb4d29b0b334f03d4fce
Brian Woodward <brian.woodward@gmail.com>
2016-07-29 11:47:52 -0400

    split grouping and collection file creation into separate files

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict'
 
 var utils = require('./lib/utils');
+var vgroup = require('./lib/vinyl-group');
+var vcollection = require('./lib/vinyl-collection');
 
 module.exports = function(structure, options) {
   if (typeof structure === 'object') {
@@ -14,65 +16,20 @@ module.exports = function(structure, options) {
   }
 
   var files = [];
-  var list = utils.normalizeFile(options.list);
-  var item = utils.normalizeFile(options.item);
-  var groupFn = options.groupFn || utils.noop;
   var prop = utils.getProp(structure);
-  var single = utils.single(prop);
-
-  return utils.through.obj(function(file, enc, next) {
+  return utils.through.obj(function(file, enc, cb) {
     files.push(file);
-    next(null, file);
+    cb(null, file);
   }, function(cb) {
-    try {
-      // group cached files based on provided `prop` from the `structure`.
-      var group = utils.groupArray(files, `data.${prop}`);
-      //  {
-      //    A: [<File foo.hbs>, <File bar.hbs>, <File baz.hbs>],
-      //    B: [<File foo.hbs>, <File bar.hbs>, <File baz.hbs>],
-      //    C: [<File foo.hbs>, <File bar.hbs>, <File baz.hbs>]
-      //  }
-      groupFn(group);
-
-      // if there aren't any groups, just returned
-      var keys = Object.keys(group);
-      if (keys.length === 0) {
-        return cb();
-      }
-
-      // create the main index file containing all the `groups`
-      if (list) {
-        var data = {};
-        data[prop] = prop;
-        var index = utils.createFile(list, list.structure || `:${prop}:extname`, data);
-        index.data = {};
-        index.data[prop] = group;
-        this.push(index);
-      }
-
-      // for each key (group), create a page (or pages when paginating)
-      if (item) {
-        for(var i = 0; i < keys.length; i++) {
-          var key = keys[i];
-          var items = group[key];
-          var opts = {structure: structure, prop: single};
-          opts[prop] = prop;
-          opts[single] = key;
-
-          if (options.paginate) {
-            opts.paginate = options.paginate;
-            utils.paginate(item, items, opts, this.push.bind(this));
-          } else {
-            var file = utils.createFile(item, structure, opts);
-            file.data = {items: items};
-            file.data[single] = key;
-            this.push(file);
-          }
-        }
-      }
-    } catch (err) {
-      this.emit('error', new utils.PluginError('gulp-collection', err, options));
+    var group = vgroup.groupBy(files, `data.${prop}`);
+    if (typeof options.groupFn === 'function') {
+      options.groupFn(group);
     }
+
+    files = vcollection.createFiles(group, structure, options);
+    files.forEach(function(file) {
+      this.push(file);
+    }, this);
     cb();
   });
 };
